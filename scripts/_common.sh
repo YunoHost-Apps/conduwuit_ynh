85fbd09389e3ba7e4c984c7dd5727da072faad1a
loowiz <loowiz@envs.net>
2024-12-28 21:17:36 +0000

    Test moving registration_token initialisation to install script

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,6 +9,3 @@ if [ "$registration" -eq 1 ]; then
 else
     registration="false"
 fi
-
-registration_token=$(ynh_string_random --length=8)
-ynh_app_setting_set --key=registration_token --value="$registration_token"
