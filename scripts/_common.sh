d35e1cab6416c66d18765bee4f89ba35f8c2f545
loowiz <loowiz@envs.net>
2025-01-03 22:48:10 +0000

    Fix extra config panel options, fix coturn, fix sliding sync

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-if [ "$registration" -eq 1 ]; then
+if [ "${registration:-}" -eq 1 ]; then
     registration="true"
 else
     registration="false"
