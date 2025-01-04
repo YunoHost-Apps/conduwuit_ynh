1c55642d85c692316df323b4e33016a4d08eeeba
loowiz <loowiz@envs.net>
2025-01-04 00:51:51 +0000

    Bump version, add sync_users config, fix hook

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -14,13 +14,13 @@ fi
 # CUSTOM HELPERS
 #=================================================
 
+# NOTE: PLEASE APPLY CHANGES TO THIS FUNCTION TO THE ONE IN post_user_create TOO
 myynh_add_user() {
   local username="$1"
   local password="$2"
   ynh_systemctl --service="$app" --action="stop" --log_path="systemd"
   cd $install_dir
-  timeout --preserve-status --foreground --kill-after=10 10 ./conduwuit -c conduwuit.toml --execute "users create-user $username $password" --execute "server shutdown"
-  # Uses the timeout command due to this issue: https://github.com/girlbossceo/conduwuit/issues/649
+  ynh_exec_and_print_stderr_only_if_error ./conduwuit -c conduwuit.toml --execute "users create-user $username $password" --execute "server shutdown"
   ynh_systemctl --service="$app" --action="start" --log_path="systemd"
 }
 
@@ -28,7 +28,6 @@ myynh_deactivate_user() {
   local username="$1"
   ynh_systemctl --service="$app" --action="stop" --log_path="systemd"
   cd $install_dir
-  timeout --preserve-status --foreground --kill-after=10 10 ./conduwuit -c conduwuit.toml --execute "users deactivate $username" --execute "server shutdown"
-  # Uses the timeout command due to this issue: https://github.com/girlbossceo/conduwuit/issues/649
+  ./conduwuit -c conduwuit.toml --execute "users deactivate $username" --execute "server shutdown"
   ynh_systemctl --service="$app" --action="start" --log_path="systemd"
 }
